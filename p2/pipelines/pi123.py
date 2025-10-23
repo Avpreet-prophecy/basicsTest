025aa110aebd70103662cf895dfb0f7ca5569011
avpreet@prophecy.io <avpreet@prophecy.io>
2025-10-23 12:49:51 +0000

    Add new pipeline `pi123` and macro `prophecy_tmp_source` in project `p2`

---
+++ p2/pipelines/pi123.py
@@ -0,0 +1,5 @@
+Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
+SensorSchedule = SensorSchedule(enabled = False)
+
+with DAG(Schedule = Schedule, SensorSchedule = SensorSchedule):
+    m1_0 = Task(task_id = "m1_0", component = "Model", modelName = "m1")
